7778af3826f11f2ee43a9f406129f3c71e1a03a8
glebik <152169562+Ebobolo@users.noreply.github.com>
2023-12-13 20:42:27 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,27 @@
+#№1
+# num1 = int(input("введите число 1\n"))
+# num2 = int(input("введите число 2\n"))
+# num3 = int(input("введите число 3\n"))
+# all = [num1, num2, num3]
+#
+# c = int(input("выберите действие: 1 найти максимум, 2 найти минимум, 3 найти среднее \n"))
+#
+# if c == 1:
+#     print ('Максимум из 3х:', max(all))
+# elif c == 2:
+#     print ('Минимум из 3х:', min(all))
+# elif c == 3:
+#     print('Среднее из 3х:', sum(all) / len(all))
+# else:
+#     print("введите число от 1 до 3")
+#_________________________________________________________________________________________________
+#№3
+# def calculate_area(d1, d2):
+#     return 0.5 * d1 * d2
+#
+# d1 = float(input("Enter the length of the first diagonal: "))
+# d2 = float(input("Enter the length of the second diagonal: "))
+#
+# area = calculate_area(d1, d2)
+#
+# print("The area of the rhombus is", area)
